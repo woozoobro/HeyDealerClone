406c33bc1764fb442bac903ffcf63b98a0343a0e
woozoobro <woojusm@gmail.com>
2023-10-14 00:33:16 +0900

    [ADD] initial commit

---
+++ HeyDealerClone/HeyDealerCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeyDealerCloneApp.swift
+//  HeyDealerClone
+//
+//  Created by 우주형 on 2023/10/14.
+//
+
+import SwiftUI
+
+@main
+struct HeyDealerCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
