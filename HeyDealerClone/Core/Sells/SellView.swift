2ab018f70db3cde1b65246619043ab531701cc15
woozoobro <woojusm@gmail.com>
2023-10-29 21:50:50 +0900

    [ADD] SellViewModel Combine

--- HeyDealerClone/Core/Sells/SellView.swift
+++ HeyDealerClone/Core/Sells/SellView.swift
@@ -6,23 +6,44 @@
 //
 
 import SwiftUI
+import Combine
 
 final class SellViewModel: ObservableObject {
     
     @Published var search: Bool = false
+    @Published var showNavigation: Bool = false
     
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        setupSubscribers()
+    }
+    
+    private func setupSubscribers() {
+        $search
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] isSearching in
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    self?.showNavigation = isSearching
+                }
+            }
+            .store(in: &cancellables)
+    }
 }
 
 struct SellView: View {
     @StateObject private var vm: SellViewModel = SellViewModel()
     @State private var text: String = ""
+    @Namespace private var namespace
+//    @State private var path: NavigationPath = .init()
     
     
     var body: some View {
         VStack(spacing: 40) {
             MainTitle(title: "먼저, 내 차 시세를\n알아볼까요?", fontColor: .theme.title)
-                
+            
             LicenseTextField(text: $text, search: $vm.search)
+                .matchedGeometryEffect(id: "LicenseTextField", in: namespace)
             
             Spacer()
             
@@ -31,12 +52,13 @@ struct SellView: View {
                     .transition(.move(edge: .bottom))
             }
         }
-        
     }
 }
 
 struct LoadingToast: View {
     @State private var loading: Bool = false
+    @State private var trim1: Bool = false
+    @State private var trim2: Bool = false
     var body: some View {
         RoundedRectangle(cornerRadius: 8)
             .fill(Color.blue)
@@ -50,12 +72,19 @@ struct LoadingToast: View {
                     }
                     Spacer()
                     Circle()
-                        .trim(from: 0, to: 0.9)
+                        .trim(from: 0, to: trim1 ? 1 : 0)
+                        .trim(from: trim2 ? 1 : 0, to: 1)
                         .stroke(lineWidth: 2)
                         .frame(width: 16)
-                        .rotationEffect(.init(degrees: loading ? 360 : 0))
-                        .animation(.easeInOut(duration: 1.2).repeatForever(autoreverses: false), value: loading)
-                        .onAppear { loading.toggle() }
+                        .rotationEffect(.init(degrees: loading ? 180 : 0))
+                        .animation(.easeInOut(duration: 1.4).repeatForever(autoreverses: false), value: loading)
+                        .animation(.easeInOut(duration: 2).repeatForever(autoreverses: false), value: trim1)
+                        .animation(.easeInOut(duration: 4).repeatForever(autoreverses: true), value: trim2)
+                        .onAppear {
+                            loading.toggle()
+                            trim1.toggle()
+                            trim2.toggle()
+                        }
                 }
                 .customFont(fontWeight: .bold, size: 20)
                 .padding(20)
